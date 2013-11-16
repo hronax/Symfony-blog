6c624b2929696539f7b03158c1e6d6b5cb565324
zhron4x <zhr0n4x@gmail.com>
2013-11-16 13:32:19 +0300

    add email form

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -14,30 +14,42 @@ class PageController extends Controller
     {
         return $this->render('BloggerBlogBundle:Page:index.html.twig');
     }
-	public function aboutAction()
+
+    public function aboutAction()
     {
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
     }
-	public function contactAction()
-	{
-		$enquiry = new Enquiry();
-		$form = $this->createForm(new EnquiryType(), $enquiry);
 
-		$request = $this->getRequest();
-		if ($request->getMethod() == 'POST') {
-			$form->submit($request);
+    public function contactAction()
+    {
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
 
-			if ($form->isValid()) {
-				// Perform some action, such as sending an email
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->submit($request);
 
-				// Redirect - This is important to prevent users re-posting
-				// the form if they refresh the page
-				return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
-			}
-		}
+            if ($form->isValid()) {
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
 
-		return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
-			'form' => $form->createView()
-		));
-	}
+                $this->get('session')->getFlashBag()->add(
+                    'blogger-notice',
+                    'Your contact enquiry was successfully sent. Thank you!'
+                );
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
 }
