5d3b8b594ff7c242751299c4368c1e14f678505d
zhron4x <zhr0n4x@gmail.com>
2013-11-23 17:33:00 +0200

    create admin bundle

--- src/Blogger/AdminBundle/Controller/DefaultController.php
+++ src/Blogger/AdminBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('BloggerAdminBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('BloggerAdminBundle:Default:index.html.twig');
+    }
+
+    public function sidebarAction()
+    {
+        return $this->render('BloggerAdminBundle:Default:sidebar.html.twig');
     }
 }
